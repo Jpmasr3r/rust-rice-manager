08fe38de378f5b4ea77120d2b49d125d6b94fdc1
Jpmasr3r <jpanjolimsoares@gmail.com>
2025-10-04 16:19:28 -0300

    Feature: Added the app logic

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # rust-rice-manager
+
+## ***Commands***
+### app : Manipula Apps
+- add : adiciona algo dependendo do comando
+- delete : apaga algo dependendo do comando
+- update : Atualiza algo dependendo do comando
+### file : Manipula Arquivos
+- add : adiciona algo dependendo do comando
+- delete : apaga algo dependendo do comando
+- update : Atualiza algo dependendo do comando
+### change : Muda o rice atual
+- -n, --number : numero do rice
+### list : Lista os rices salvos
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use clap::Parser;
 fn main() {
-    println!("Hello, world!");
+    let rrm = Args::parse();
+
+    match rrm.sub_args {
+        SubArgs::App { function } => {
+            println!("Modificando App");
+            match function {
+                Crud::Add { name } => {
+                    println!("Novo App adicionado => {}", name);
+                }
+                Crud::Delete { name } => {
+                    println!("App removido => {}", name);
+                }
+                Crud::Update { name, new_name } => {
+                    println!("App atualizado => {} para {}", name, new_name);
+                }
+                Crud::List => {
+                    println!("Lista de apps => app1,app2,app3, etc...")
+                }
+            }
+        }
+        _ => {}
+    }
+}
+
+#[derive(clap::Parser)]
+#[command(name = "rrm")]
+#[command(about = "RRM is a CLI rice manager")]
+#[command(version = "0.0.1")]
+#[command(author = "Jpmasr3r")]
+struct Args {
+    #[command(subcommand)]
+    sub_args: SubArgs,
+}
+
+#[derive(clap::Subcommand)]
+enum SubArgs {
+    App {
+        #[command(subcommand)]
+        function: Crud,
+    },
+    File,
+    Change,
+    List,
+}
+
+#[derive(clap::Subcommand)]
+enum Crud {
+    Add {
+        #[arg(long = "name", short = 'n')]
+        name: String,
+    },
+    List,
+    Update {
+        #[arg(long = "name", short = 'n')]
+        name: String,
+        #[arg(long = "new-name")]
+        new_name: String,
+    },
+    Delete {
+        #[arg(long = "name", short = 'n')]
+        name: String,
+    },
 }
