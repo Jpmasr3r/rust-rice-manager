ca0ca3d943a22250d7c22dd96403b60d5dec84d9
Jpmasr3r <jpanjolimsoares@gmail.com>
2025-10-05 17:41:28 -0300

    feature: apps logic

---
+++ src/json.rs
@@ -0,0 +1,96 @@
+//JSON file manipulation functions
+
+use std::{fs, io::Write};
+
+use serde::{Deserialize, Serialize};
+
+const PATH_TO_CONFIG_FILE: &str = "config.json";
+
+pub fn read_json() -> Data {
+    let content = fs::read_to_string(PATH_TO_CONFIG_FILE).unwrap_or_else(|_| {
+        // se não existir, cria o padrão
+        let default_data = Data::default();
+        let json = serde_json::to_string_pretty(&default_data).unwrap();
+        let mut file = std::fs::File::create(PATH_TO_CONFIG_FILE).unwrap();
+        file.write_all(json.as_bytes()).unwrap();
+        json
+    });
+
+    // tenta converter o conteúdo JSON em Data
+    serde_json::from_str(&content).unwrap_or_else(|_| {
+        // se der erro ao parsear, recria o arquivo padrão
+        let default_data = Data::default();
+        let json = serde_json::to_string_pretty(&default_data).unwrap();
+        let mut file = std::fs::File::create(PATH_TO_CONFIG_FILE).unwrap();
+        file.write_all(json.as_bytes()).unwrap();
+        default_data
+    })
+}
+
+pub fn json_write(data: Data) -> std::io::Result<()> {
+    let path = &PATH_TO_CONFIG_FILE;
+    let json = serde_json::to_string_pretty(&data).unwrap();
+    let mut file = std::fs::File::create(path)?;
+    file.write_all(json.as_bytes())?;
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Data {
+    pub apps: Vec<App>,
+    pub files: Vec<File>,
+    pub rices: Vec<Rice>,
+    pub config: Config,
+}
+
+impl Data {
+    fn default() -> Self {
+        Self {
+            apps: Vec::new(),
+            files: Vec::new(),
+            rices: Vec::new(),
+            config: Config::default(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct App {
+    pub name: String,
+}
+
+impl App {
+    pub fn from(name: String) -> Self {
+        Self { name }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct File {
+    pub path: String,
+    pub app: App,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Rice {
+    pub name: String,
+    pub files_paths: Vec<Path>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub rices_path: String,
+}
+
+impl Config {
+    pub fn default() -> Self {
+        Self {
+            rices_path: String::from("rices"),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Path {
+    pub path: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,67 @@
 use clap::Parser;
+
+use crate::json::{json_write, read_json};
+
+mod json;
 fn main() {
     let rrm = Args::parse();
 
     match rrm.sub_args {
         SubArgs::App { function } => {
-            println!("Modificando App");
+            println!("Entrando em App");
             match function {
                 Crud::Add { name } => {
-                    println!("Novo App adicionado => {}", name);
+                    let mut data = read_json();
+
+                    if data.apps.iter().any(|x| x.name == name) {
+                        println!("App já existe!");
+                        return;
+                    }
+
+                    data.apps.push(json::App::from(name.clone()));
+                    match json_write(data) {
+                        Ok(_) => println!("Novo App {} adicionado", name),
+                        Err(e) => eprintln!("Erro ao salvar JSON: {}", e),
+                    };
                 }
                 Crud::Delete { name } => {
-                    println!("App removido => {}", name);
+                    let mut data = read_json();
+
+                    if !data.apps.iter().any(|x| x.name == name) {
+                        println!("App não existe!");
+                        return;
+                    }
+
+                    if let Some(pos) = data.apps.iter().position(|x| x.name == name) {
+                        data.apps.remove(pos);
+                    }
+                    match json_write(data) {
+                        Ok(_) => println!("App {} removido", name),
+                        Err(e) => eprintln!("Erro ao salvar JSON: {}", e),
+                    };
                 }
                 Crud::Update { name, new_name } => {
-                    println!("App atualizado => {} para {}", name, new_name);
+                    let mut data = read_json();
+
+                    if !data.apps.iter().any(|x| x.name == name) {
+                        println!("App original não existe!");
+                        return;
+                    }
+
+                    if let Some(pos) = data.apps.iter().position(|x| x.name == name) {
+                        data.apps[pos].name = new_name.clone();
+                    }
+                    match json_write(data) {
+                        Ok(_) => println!("App {} atualizado para {}", name, new_name),
+                        Err(e) => eprintln!("Erro ao salvar JSON: {}", e),
+                    };
                 }
                 Crud::List => {
-                    println!("Lista de apps => app1,app2,app3, etc...")
+                    println!("Lista de Apps");
+                    let data = read_json();
+                    for app in data.apps {
+                        println!("{}", app.name);
+                    }
                 }
             }
         }
